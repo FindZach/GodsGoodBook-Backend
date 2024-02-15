735f366e5344bf49ccf6a3ed8edcf3e4f9c74238
Zach <zach@findzach.com>
2024-02-14 22:25:55 -0500

    Added frame for Job Tracking

---
+++ src/main/java/com/findzach/godsgoodbook/tools/JobDetails.java
@@ -0,0 +1,13 @@
+package com.findzach.godsgoodbook.tools;
+
+/**
+ * @author: Zach Smith
+ * @date: 2/14/2024
+ * @time: 10:08 PM
+ *
+ * Will display details about the current job
+ */
+public interface JobDetails {
+
+
+}
